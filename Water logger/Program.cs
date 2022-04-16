e9426a861b7a7e9cdb633a46b30967cdc2f1e70a
Matt Sella <guppyflies@gmail.com>
2022-04-16 10:34:05 -0600

    All done except for validation.

--- Water logger/Program.cs
+++ Water logger/Program.cs
@@ -30,10 +30,10 @@ namespace Water_logger
          static void GetUserInput()
         {
             Console.Clear();
-           
             bool closeApp = false;
             while (closeApp == false)
             {
+                Console.WriteLine("MAIN MENU\n");
                 Console.WriteLine("Please select an option");
                 Console.WriteLine("\n----------------------");
                 Console.WriteLine("\nType 0 to Close Application");
@@ -49,6 +49,7 @@ namespace Water_logger
                     case "0":
                         Console.WriteLine("Goodbye!");
                         closeApp = true;
+                        Environment.Exit(0);
                         break;
                     
                     case "1":
@@ -66,68 +67,22 @@ namespace Water_logger
                     case "4":
                         Delete();
                         break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter a number 0 - 4");
+                        break;
                 }
-                Environment.Exit(0);
-            }
-
-
-        }
-        private static void Insert()
-        {
-            string date = GetDateInput();
-
-            int quantity = GetNumberInput("Please enter quantity of water consumed. Press 0 to return to Main Menu.");
-
-            using (var connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-
-                var tableCommand = connection.CreateCommand();
-
-                tableCommand.CommandText = $"INSERT INTO drinking_water (Date, Quantity) VALUES ('{date}',{quantity})";
                 
-                tableCommand.ExecuteNonQuery();
-
-                connection.Close();
             }
 
-            Console.WriteLine($"Data successfully added with {date} as date and {quantity} as quantity. Press any key to continue.");
-
-            Console.ReadKey();
-
-            GetUserInput();
-
-        }
-        private static string GetDateInput()
-        {
-            Console.WriteLine("Please enter a date in MM-DD-YY format. Press 0 to return to Main Menu.");
-           
-            string input = Console.ReadLine();
-
-            if (input == "0")
-                GetUserInput();
-
-            return input;
-        }
-        private static int GetNumberInput(string message)
-        {
-            Console.WriteLine(message);
-
-            string input = Console.ReadLine();
-
-            if (input == "0")
-                GetUserInput();
-
-            return Convert.ToInt32(input);
 
         }
         private static void GetAllRecords()
         {
             Console.Clear();
-            using(var connection = new SqliteConnection(connectionString))
+            using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
-                
+
                 var tableCommand = connection.CreateCommand();
 
                 tableCommand.CommandText = @"SELECT * FROM drinking_water";
@@ -146,34 +101,53 @@ namespace Water_logger
                             Date = DateTime.ParseExact(reader.GetString(1), "MM-dd-yy", new CultureInfo("en-US")),
                             Quantity = reader.GetInt32(2)
 
-                        }) ; ;
+                        }); ;
 
                     }
-                
+
                 }
                 else
                     Console.WriteLine("No data found");
 
                 connection.Close();
 
-                Console.WriteLine("\n---------------");
+                Console.WriteLine("\n--------------------");
 
-                foreach(var i in tableData)
+                foreach (var i in tableData)
                 {
                     Console.WriteLine($"{i.Id} -- {i.Date.ToString("MM-dd-yy")} -- Quantity: {i.Quantity}");
                 }
 
-                Console.WriteLine("\n---------------");
+                Console.WriteLine("\n--------------------");
 
             }
+        }
+        private static void Insert()
+        {
+            string date = GetDateInput();
+
+            int quantity = GetNumberInput("Please enter quantity of water consumed (no decimals allowed). Press 0 to return to Main Menu.");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var tableCommand = connection.CreateCommand();
+
+                tableCommand.CommandText = $"INSERT INTO drinking_water (Date, Quantity) VALUES ('{date}',{quantity})";
+                
+                tableCommand.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
         }
         private static void Delete()
         {
             Console.Clear();
-            
             GetAllRecords();
 
-            var inputId = GetNumberInput("Enter ID of record you would to delete. Type 0 to return to main menu.");
+            var inputId = GetNumberInput("Enter ID of record you would like to delete. Type 0 to return to main menu.");
 
             using(var connection = new SqliteConnection(connectionString))
             {
@@ -187,24 +161,85 @@ namespace Water_logger
 
                 if (rowCount == 0)
                 {
-                    Console.WriteLine("Record does not exist");
+                    Console.WriteLine("Record does not exist. Press any key to try again.");
+                    Console.ReadKey();
                     Delete();
                 }
                     
                 else
-                    Console.WriteLine("Record was deleted");
-                
+                {
+                    Console.WriteLine("Record was deleted. Press any key to continue");
+                    Console.ReadKey();
+                }
+
                 connection.Close();
+
             }
             GetUserInput();
         }
         private static void Update()
         {
+            Console.Clear();
+
+            GetAllRecords();
+
+            var inputId = GetNumberInput("Enter ID of record you would like to update. Type 0 to return to main menu.");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var tableCommand = connection.CreateCommand();
+
+                string date = GetDateInput();
+
+                int quantity = GetNumberInput("Enter updated quantity (no decimals allowed). Type 0 to return to main menu.");
+
+                tableCommand.CommandText = $"UPDATE drinking_water SET Date = '{date}', Quantity = {quantity} WHERE Id = {inputId}";
+
+                int rowCount = tableCommand.ExecuteNonQuery();
+
+                if (rowCount == 0)
+                {
+                    Console.WriteLine("Record not exist. Please try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"Record {inputId} was updated with date as {date} and quantity as {quantity}");
+                    Console.WriteLine("\n\nPress any key to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+        internal static int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+
+            string input = Console.ReadLine();
+
+            if (input == "0")
+            {
+                Console.Clear();
+                GetUserInput();
+            }
+
+
+            return Convert.ToInt32(input);
 
         }
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("Please enter a date in MM-DD-YY format. Press 0 to return to Main Menu.");
 
+            string input = Console.ReadLine();
+
+            if (input == "0")
+                GetUserInput();
+
+            return input;
+        }
     }
-
     public class drinkingwater
     {
         public int Id { get; set; }
